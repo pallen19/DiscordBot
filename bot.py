808831d338d31e35358052b684370daf0140d06c
Paul Allen <Lancea818@gmail.com>
2023-06-12 19:18:15 -0400

    basic bot functions

--- bot.py
+++ bot.py
@@ -5,14 +5,14 @@ import responses
 
 async def send_message(message, user_message, is_private):
     try:
-        response: response.get_response(user_message)
+        response = responses.get_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
 
     except Exception as e:
         print(e)
 
 def run_discord_bot():
-    TOKEN = 'MTExNzkzNDgxMjU2OTgwMDcwNQ.G9rFPb.qqLkhzdAlm7qp6hIqXnyAfuHGxbKBtkMco8Ozo'
+    TOKEN = 'Your Token ID'
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
@@ -25,7 +25,7 @@ def run_discord_bot():
     @client.event
     async def on_message(message):
         if message.author == client.user:
-            return 'name'
+            return 
         
         username = str(message.author)
         user_message = str(message.content)
@@ -38,6 +38,7 @@ def run_discord_bot():
         if user_message[0] == '?':
             user_message = user_message[1:]
             await send_message(message, user_message, is_private=True)
-        else: await send_message(message, user_message, is_private=False)
+        else: 
+            await send_message(message, user_message, is_private=False)
 
     client.run(TOKEN)
